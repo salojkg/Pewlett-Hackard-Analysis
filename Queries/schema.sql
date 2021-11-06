a74d9a220feaae983b34f2125b9fe5b70dc6d058
salojkg <90934630+salojkg@users.noreply.github.com>
2021-11-06 10:30:02 -0500

    Retirement_info

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -41,3 +41,31 @@ CREATE table titles (
 	foreign key (emp_no) references employees (emp_no),
 	primary key (emp_no, from_date)
 );
+
+SELECT * from departments;
+SELECT * from dept_manager;
+SELECT * from employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Retirement Info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
